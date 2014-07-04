af4f6eb5e4e0dd11323d8f7769a4a9379152d955
abbat <antonbatenev@yandex.ru>
2014-07-04 18:58:20 +0400

    Обновление скрипта сборки для Debian на последнюю версию

--- debian.sh
+++ debian.sh
@@ -4,7 +4,7 @@
 # скрипт для быстрой сборки nginx-full из backports
 #
 
-SRC_URL="http://mirror.yandex.ru/debian-backports/pool/main/n/nginx/nginx_1.2.1-2.2~bpo60%2b2.dsc"
+SRC_URL="http://mirror.yandex.ru/debian/pool/main/n/nginx/nginx_1.6.0-1~bpo70%2b1.dsc"
 
 WORK_DIR=$(basename "${SRC_URL}" | cut -d '~' -f 1 | cut -d '-' -f 1 | sed -e 's/_/-/')
 
